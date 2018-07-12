c56c5957fc75f973fb61f4d9b8a755b2cbc7c27a
Jake <rwenzel0@email.cpcc.edu>
2018-07-12 07:57:55 -0400

    First Commit

---
+++ venv/payment_processing.py
@@ -0,0 +1,12 @@
+import stripe
+
+stripe.api_key("sk_test_BQokikJOvBiI2HlWgH4olfQ2")
+
+card_stripe = Stripe('pk_test_g6do5S237ekq10r65BnxO6S0')
+
+card = elements.create('card')
+
+card.mount('#card-element');
+
+promise = stripe.c
+
